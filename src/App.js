98f8d16dcc0c3f0ab263526f0a21fc86cb0f5f8b
hahyuning <hahyuning@gmail.com>
2022-01-20 08:42:10 +0900

    Backend 통합

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Todo from "./Todo";
 import AddTodo from "./AddTodo";
 import "./App.css";
 import { List, Paper, Container } from "@material-ui/core";
+import { call } from "./service/ApiService";
 
 class App extends React.Component {
   constructor(props) {
@@ -12,30 +13,40 @@ class App extends React.Component {
     };
   }
 
-  add = (item) => {
-    const thisItem = this.state.items;
-    item.id = "ID-" + thisItem.length; // key를 위한 id
-    item.done = false; // done 초기화
-    thisItem.push(item); // 리스트에 아이템 추가
-    this.setState({items: thisItem}); // 업데이트
-    console.log("items : ", this.state.items);
-  }
-
-  delete = (item) => {
-    const thisItems = this.state.items;
-    console.log("Before Update Items : ", this.state.items);
-    const newItems = thisItems.filter(e => e.id !== item.id);
-    this.setState({items: newItems}, () => {
-      console.log("Update Items : ", this.state.items)
+  componentDidMount() {
+    call("/todo", "GET", null).then((response) => {
+      this.setState({ items: response.data })
     });
   }
+
+  add = (item) => {
+    call("/todo", "POST", item).then((response) => {
+      this.setState({ items: response.data })
+    });
+  };
+
+  delete = (item) => {
+    call("/todo", "DELETE", item).then((response) => {
+      this.setState({ items: response.data })
+    });
+  };
+
+  update = (item) => {
+    call("/todo", "PUT", item).then((response) => {
+      this.setState({ items: response.data })
+    });
+  };
 
   render() {
     var todoItems = this.state.items.length > 0 && (
       <Paper style={{margin: 16}}>
         <List>
           {this.state.items.map((item, idx) => (
-            <Todo item={item} key={item.id} delete={this.delete}/>
+            <Todo item={item} 
+                  key={item.id} 
+                  delete={this.delete}
+                  update={this.update}
+            />
           ))};
         </List>
       </Paper>
