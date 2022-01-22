ddf9c7cbf513e5c90170aa863e454603bb7ed6b2
hahyuning <hahyuning@gmail.com>
2022-01-22 23:49:47 +0900

    회원가입

--- src/App.js
+++ src/App.js
@@ -10,12 +10,15 @@ class App extends React.Component {
     super(props);
     this.state = { 
       items: [],
+      loading: true,
     };
   }
 
   componentDidMount() {
+    // 리스트를 가져오는 GET 요청을 성공적으로 리턴하는 경우
+    // loading을 false로 바꾼다.
     call("/todo", "GET", null).then((response) => 
-      this.setState({ items: response.data })
+      this.setState({ items: response.data, loading: false})
     );
   }
 
@@ -69,15 +72,29 @@ class App extends React.Component {
       </AppBar>
     )
 
-    return (
-      <div className="App">
-        {navigationBar}
+    // 로딩 중이 아닐 때
+    var todoListPage = (
+      <div>
+        {navigationBar} {/* 네비게이션 바 렌더링 */}
         <Container maxWidth="md">
           <AddTodo add={this.add} />
           <div className="TodoList">{todoItems}</div>
         </Container>
       </div>
-    );
+    )
+
+    // 로딩 중일 때
+    var loadingPage = <h1> 로딩중.. </h1>;
+
+    var content = loadingPage;
+
+    if (!this.state.loading) {
+      /* 로딩중이 아니면 todoListPage를 선택*/
+      content = todoListPage;
+    }
+
+    /* 선택한 content 렌더링 */
+    return <div className="App">{content}</div>;
   }
 }
 
